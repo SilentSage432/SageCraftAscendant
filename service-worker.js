cc1bc5ffdfb4909e2fb78b189c855f6abd9c8c8b
SilentSage <tys2157787@maricopa.edu>
2025-05-15 13:08:50 -0600

    updated service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,32 @@
-const CACHE_NAME = 'inventory-tool-v1';
+const CACHE_NAME = 'inventory-tool-v2'; // updated version
 const urlsToCache = [
   './',
   './index.html',
   './style.css',
   './script.js',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
